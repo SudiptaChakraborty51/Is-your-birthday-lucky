fa617d270d44b4fb3930ae4f1d9abc020aee44c0
Sudipta <hisudipta26@gmail.com>
2022-09-08 17:31:39 +0530

    feat: handled negative values

--- app.js
+++ app.js
@@ -5,44 +5,50 @@ const errorMessage = document.querySelector('.error');
 const outputMessage = document.querySelector('.output');
 const emotionGif = document.querySelector('.emotion');
 
-function replaceChar(a,b) {
-    const dob=dateOfBirth.value;
-    let result="";
-    for(let i=0; i<dob.length; i++){
-        const ch=dob.charAt(i);
-        if(ch===a){
-            result+=b;
+function replaceChar(a, b) {
+    const dob = dateOfBirth.value;
+    let result = "";
+    for (let i = 0; i < dob.length; i++) {
+        const ch = dob.charAt(i);
+        if (ch === a) {
+            result += b;
         } else {
-            result+=ch;
+            result += ch;
         }
     }
     return result;
 }
-console.log(replaceChar("-",""));
+console.log(replaceChar("-", ""));
 
 checkButton.addEventListener('click', function checkDOBLucky() {
     outputMessage.innerText = "Loading...";
-    emotionGif.style.display="none";
+    emotionGif.style.display = "none";
     if (dateOfBirth.value != "" && luckyNumber.value != "") {
-        errorMessage.style.display = "none";
-        outputMessage.style.display = "block";
-        let Dob=replaceChar("-","");
-        let sumOfDigits = 0;
-        for (let i = 0; i < Dob.length; i++) {
-            sumOfDigits = sumOfDigits + Number(Dob.charAt(i));
-        }
-        if (sumOfDigits % luckyNumber.value == 0) {
-            setTimeout(() => {
-                outputMessage.innerText = "Your Birthday is LUCKY :)";
-                emotionGif.style.display="block";
-                emotionGif.innerHTML="<img src='/images/happy-gif.gif'>";
-            },2000);
+        if (Number(luckyNumber.value) < 0) {
+            errorMessage.style.display = "block";
+            outputMessage.style.display = "none";
+            errorMessage.innerText = "Please enter positive lucky number!";
         } else {
-            setTimeout(() => {
-                outputMessage.innerText = "Your Birthday is NOT LUCKY :(";
-                emotionGif.style.display="block";
-                emotionGif.innerHTML="<img src='/images/sad-gif.gif'>";
-            },2000);
+            errorMessage.style.display = "none";
+            outputMessage.style.display = "block";
+            let Dob = replaceChar("-", "");
+            let sumOfDigits = 0;
+            for (let i = 0; i < Dob.length; i++) {
+                sumOfDigits = sumOfDigits + Number(Dob.charAt(i));
+            }
+            if (sumOfDigits % luckyNumber.value == 0) {
+                setTimeout(() => {
+                    outputMessage.innerText = "Your Birthday is LUCKY :)";
+                    emotionGif.style.display = "block";
+                    emotionGif.innerHTML = "<img src='/images/happy-gif.gif'>";
+                }, 2000);
+            } else {
+                setTimeout(() => {
+                    outputMessage.innerText = "Your Birthday is NOT LUCKY :(";
+                    emotionGif.style.display = "block";
+                    emotionGif.innerHTML = "<img src='/images/sad-gif.gif'>";
+                }, 2000);
+            }
         }
     } else {
         errorMessage.style.display = "block";
